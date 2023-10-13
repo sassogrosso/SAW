49b724b3a0561c6d8915414abd7cc9c5cbc61dab
Yuri Romano <103211870+sassogrosso@users.noreply.github.com>
2023-10-13 15:25:27 +0200

    Create registration_done.php

---
+++ RomanProject/registration_done.php
@@ -0,0 +1,7 @@
+<html>
+<?php echo '<link href="submitt.css" rel="stylesheet" type="text/css" />'; ?>
+<body >
+    <h1>Grazie per esserti registrato,ti aggiorneremo piu avanti al riguardo</h1>
+
+</body>
+</html>
